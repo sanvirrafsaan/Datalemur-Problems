6f6b2676f9f2913d91072031d78d79c77d2de7fb
Rafsaan Sanvir <90986677+sanvirrafsaan@users.noreply.github.com>
2025-04-07 15:34:00 -0400

    Create Laptop vs. Mobile Viewership.sql

---
+++ Laptop vs. Mobile Viewership.sql
@@ -0,0 +1,6 @@
+--Link to problem: https://datalemur.com/questions/laptop-mobile-viewership
+
+SELECT 
+  COUNT(*) FILTER (WHERE device_type = 'laptop') AS laptop_views,
+  COUNT(*) FILTER (WHERE device_type IN ('tablet', 'phone'))  AS mobile_views 
+FROM viewership;
