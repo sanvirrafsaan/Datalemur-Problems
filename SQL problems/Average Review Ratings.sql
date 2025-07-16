6cc4189716629e7bb45cc8257d643ce744ba5226
Rafsaan Sanvir <90986677+sanvirrafsaan@users.noreply.github.com>
2025-07-16 15:15:36 -0400

    Create Average Review Ratings.sql

---
+++ SQL problems/Average Review Ratings.sql
@@ -0,0 +1,10 @@
+-- link to problem: https://datalemur.com/questions/sql-avg-review-ratings
+
+SELECT EXTRACT(MONTH FROM submit_date) AS mth,
+  product_id AS product, 
+  ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews 
+GROUP BY 
+  EXTRACT(MONTH FROM submit_date), 
+  product_id
+ORDER BY mth, product_id;
